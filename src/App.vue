6864dc2f247530bf79c2f2a52f7a35618efcb3f9
EftekharAlam2 <hinosuvlmare@gmail.com>
2024-06-05 15:56:06 +0600

    computed properties added

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import moment from 'moment';
 
 const posts = ref([
@@ -32,7 +32,16 @@ const posts = ref([
   }
 ]);
 
-const userName = ref('Eftekhar Alam');
+const firstName = "Eftekhar";
+const lastName = "Alam";
+const userName = computed(() => {
+  return firstName + " " + lastName;
+})
+// const userName = ref('Eftekhar Alam');
+
+const reversePosts = computed(() => {
+  return [...posts.value].reverse();
+})
 
 function likeCount(index){
   posts.value[index].likes++;
@@ -147,7 +156,7 @@ function showingComments(index){
       </div>
 
       <div class="row">
-        <div class="col-md-4" v-for="(post, index) in posts" :key="post.id">
+        <div class="col-md-4" v-for="(post, index) in reversePosts" :key="post.id">
           <div class="card mb-4">
             <div class="card-body">
               <h5 class="card-title">{{ post.title }}</h5>
@@ -157,7 +166,7 @@ function showingComments(index){
                 <small v-if="post.likes>1"> {{post.likes}} likes</small>
                 <small v-else-if="post.likes==1">{{post.likes}} like</small>
                 <small v-else>No Like</small>
-                <span @click="showingComments(index)">
+                <span @click="showingComments(reversePosts.length - (index+1))">
                   <small v-if="post.comments.length>1">, {{ post.comments.length }} comments</small>
                   <small v-else-if="post.comments.length==1">, {{ post.comments.length }} comment</small>
                   <small v-else>, No Comment</small>
@@ -166,21 +175,21 @@ function showingComments(index){
 
               <div class="comments mb-3">
                 <div class="comments-input d-flex mb-3">
-                  <input type="text" class="form-control form-control-sm me-2" placeholder="Write Comment" v-model="commentInputs[index]">
-                  <button class="btn btn-sm btn-success" @click="submitComment(index)"><i class="bi bi-send"></i></button>
+                  <input type="text" class="form-control form-control-sm me-2" placeholder="Write Comment" v-model="commentInputs[reversePosts.length - (index+1)]">
+                  <button class="btn btn-sm btn-success" @click="submitComment(reversePosts.length - (index+1))"><i class="bi bi-send"></i></button>
                 </div>
 
                 <div v-if="post.showing">
                   <div class="comment mb-3 ms-3" v-for="(comment, count) in post.comments" :key="comment.id">
-                    <h6 class="card-title small"> {{comment.user}} <span class="text-danger float-end cursor-pointer" @click="deleteComment(index, count)">X</span></h6>
+                    <h6 class="card-title small"> {{comment.user}} <span class="text-danger float-end cursor-pointer" @click="deleteComment(reversePosts.length - (index+1), count)">X</span></h6>
                     <p class="card-subtitle mb-1 text-body-secondary small"> {{moment(comment.date).fromNow()}} </p>
                     <p class="card-text small"> {{comment.comment}} </p>
                   </div>
                 </div>
               </div>
 
-              <button class="btn btn-sm btn-primary" @click="likeCount(index)">Like</button>
-              <button class="btn btn-sm btn-danger float-end" @click="deleteC(index)"><i class="bi bi-trash"></i></button>
+              <button class="btn btn-sm btn-primary" @click="likeCount(reversePosts.length - (index+1))">Like</button>
+              <button class="btn btn-sm btn-danger float-end" @click="deleteC(reversePosts.length - (index+1))"><i class="bi bi-trash"></i></button>
             </div>
           </div>
         </div>
