ce3addaf17b0dde67821d65e0be0caca47e4ccaf
EftekharAlam2 <hinosuvlmare@gmail.com>
2024-06-12 16:43:54 +0600

    content layout setup

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { ref, computed, watch } from 'vue';
 import moment from 'moment';
 import Posts from './components/Posts.vue';
 import CreatePost from './components/CreatePost.vue';
+import LayoutContent from './components/LayoutContent.vue';
 
 const posts = ref([
   {
@@ -114,47 +115,18 @@ watch(
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-    <div class="container">
-      <a class="navbar-brand" href="#">BAT Blog</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-        </ul>
-        <ul class="navbar-nav mb-2 mb-lg-0">
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              {{userName}}
-            </a>
-            <ul class="dropdown-menu dropdown-menu-end">
-              <li><a class="dropdown-item" href="#">Profile</a></li>
-              <li><a class="dropdown-item" href="#">Logout</a></li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+  <LayoutContent :userName="userName">
+    <CreatePost @submitData="submitData" :postCount="posts.length" />
 
-  <CreatePost @submitData="submitData" :postCount="posts.length" />
-
-  <Posts :posts="posts"
-    :commentInputs="commentInputs"
-    @likeCount="likeCount"
-    @submitComment="submitComment"
-    @deleteComment="deleteComment"
-    @deleteC="deleteC"
-    @showingComments="showingComments"
-   />
+    <Posts :posts="posts"
+      :commentInputs="commentInputs"
+      @likeCount="likeCount"
+      @submitComment="submitComment"
+      @deleteComment="deleteComment"
+      @deleteC="deleteC"
+      @showingComments="showingComments"
+    />
+  </LayoutContent>  
 </template>
 
 <style scoped>
